66f9968bb3be0a8e806451384a971f4f49e17831
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2023-09-11 10:49:48 +0200

    add publish script

---
+++ publish.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+echo $1 | grep -E -q '^[0-9]+\.[0-9]+(\.[0-9]+).*?$' || die "Semantic Version argument required, $1 provided"
+
+[[ -z $(git status -s) ]] || die "git status is not clean"
+
+export TAG=$1
+
+# gradle -Pversion="$TAG" publish
+gradle jsBrowserProductionWebpack
+
+#echo "upload to gcloud"
+#gsutil -m rsync -r localRepo gs://mvn-public-tryformation/releases
+
+echo "tagging"
+git tag "$TAG"
+
+echo "publishing $TAG"
+
+git push --tags
+
+rsync -azpv --exclude maven* --exclude bmath --delete-after  build/dist/js/productionExecutable/* jillesvangurpcom@ftp.jillesvangurp.com:/srv/home/jillesvangurpcom/domains/jillesvangurp.com/htdocs/rankquest
