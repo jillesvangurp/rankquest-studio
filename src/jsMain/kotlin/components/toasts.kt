812e1c6bbce6e0f65ad1fe3c26cfb29c9096c504
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2024-11-09 12:04:46 +0100

    don't let it use li with a bullet, sigh

--- src/jsMain/kotlin/components/toasts.kt
+++ src/jsMain/kotlin/components/toasts.kt
@@ -5,7 +5,7 @@ import dev.fritz2.headless.components.toast
 import kotlin.time.Duration.Companion.seconds
 
 fun infoBubble(text: String) {
-    toast("messages", duration = 3.seconds.inWholeMilliseconds, ) {
+    toast("messages", duration = 3.seconds.inWholeMilliseconds, tag = RenderContext::div ) {
         div("bg-blueBright-200 p-5 rounded-lg border-2 border-blueBright-400 text-center w-96") {
             +text
         }
@@ -13,7 +13,7 @@ fun infoBubble(text: String) {
 }
 
 fun warningBubble(text: String) {
-    toast("messages", duration = 6.seconds.inWholeMilliseconds, ) {
+    toast("messages", duration = 6.seconds.inWholeMilliseconds, tag = RenderContext::div ) {
         div("bg-yellow-200 p-5 rounded-lg border-2 border-yellow-400 text-center w-96") {
             +text
         }
@@ -24,7 +24,7 @@ fun errorBubble(text: String, e: Throwable?=null) {
     if(e!=null) {
         console.error(e)
     }
-    toast("messages", duration = 10.seconds.inWholeMilliseconds, ) {
+    toast("messages", duration = 10.seconds.inWholeMilliseconds, tag = RenderContext::div) {
         div("bg-red-200 p-5 rounded-lg border-2 border-red-400 text-center w-96") {
             +text
             e?.let {
